7733b19940f182e4db00428fdba3fc5261141919
771073216 <771073216@qq.com>
2020-11-20 04:04:18 -0800

    update

--- all.sh
+++ all.sh
@@ -64,39 +64,6 @@ set_config() {
 EOF
 }
 
-choice() {
-  echo -e "1.vmess+tsp 2.vmess+h2c 3.vless+tsp 4.vless+h2c"
-  echo -e -n "[${green}Info${plain}] 输入： "
-  read -r selected
-}
-
-install_v2() {
-  curl -sSL https://apt.v2fly.org/pubkey.gpg | sudo apt-key add -
-  echo "deb [arch=amd64] https://apt.v2fly.org/ stable main" | sudo tee /etc/apt/sources.list.d/v2ray.list
-  apt update
-  apt install v2ray -y
-}
-
-install_tsp() {
-  echo -e "[${green}Info${plain}] 开始安装${yellow}tls-shunt-proxy${plain}"
-  wget -c "https://api.azzb.workers.dev/$tsplink"
-  wget -qP /etc/systemd/system/ 'https://cdn.jsdelivr.net/gh/liberal-boy/tls-shunt-proxy@master/dist/tls-shunt-proxy.service'
-  unzip -oq "tls-shunt-proxy-linux-amd64.zip"
-  install -m 755 "tls-shunt-proxy" /usr/local/bin/
-  if ! grep < /etc/passwd tls-shunt-proxy; then
-    useradd tls-shunt-proxy -s /usr/sbin/nologin
-  fi
-  install -d -o tls-shunt-proxy -g tls-shunt-proxy /etc/ssl/tls-shunt-proxy/
-  echo -e "[${green}Info${plain}] tls-shunt-proxy完成安装！"
-}
-
-install_caddy() {
-  if ! command -v "caddy" > /dev/null 2>&1; then
-    echo "deb [trusted=yes] https://apt.fury.io/caddy/ /" > /etc/apt/sources.list.d/caddy-fury.list
-    apt update && apt install caddy
-  fi
-}
-
 set_tsp() {
   cat > /etc/tls-shunt-proxy/config.yaml <<- EOF
 listen: 0.0.0.0:443
@@ -140,22 +107,56 @@ set_bbr() {
   sysctl -p > /dev/null 2>&1
 }
 
+choice() {
+  echo -e "1.vmess+tsp 2.vmess+h2c 3.vless+tsp 4.vless+h2c"
+  echo -e -n "[${green}Info${plain}] 输入： "
+  read -r selected
+}
+
+install_v2() {
+  curl -sSL https://apt.v2fly.org/pubkey.gpg | sudo apt-key add -
+  echo "deb [arch=amd64] https://apt.v2fly.org/ stable main" | sudo tee /etc/apt/sources.list.d/v2ray.list
+  apt update
+  apt install v2ray -y
+}
+
+install_tsp() {
+  echo -e "[${green}Info${plain}] 开始安装${yellow}tls-shunt-proxy${plain}"
+  wget -c "https://api.azzb.workers.dev/$tsplink"
+  wget -qP /etc/systemd/system/ 'https://cdn.jsdelivr.net/gh/liberal-boy/tls-shunt-proxy@master/dist/tls-shunt-proxy.service'
+  unzip -oq "tls-shunt-proxy-linux-amd64.zip"
+  install -m 755 "tls-shunt-proxy" /usr/local/bin/
+  if ! grep < /etc/passwd tls-shunt-proxy; then
+    useradd tls-shunt-proxy -s /usr/sbin/nologin
+  fi
+  install -d -o tls-shunt-proxy -g tls-shunt-proxy /etc/ssl/tls-shunt-proxy/
+  echo -e "[${green}Info${plain}] tls-shunt-proxy完成安装！"
+}
+
+install_caddy() {
+  if ! command -v "caddy" > /dev/null 2>&1; then
+    echo "deb [trusted=yes] https://apt.fury.io/caddy/ /" > /etc/apt/sources.list.d/caddy-fury.list
+    apt update && apt install caddy
+  fi
+}
+
 update_tsp() {
   echo -e "[${green}Info${plain}] 获取${yellow}tls-shunt-proxy${plain}版本信息..."
   tsplatest=$(wget -qO- "https://api.github.com/repos/liberal-boy/tls-shunt-proxy/releases/latest" | awk -F '"' '/tag_name/ {print $4}')
-  [ -z "${tsplatest}" ] && echo -e "[${red}Error${plain}] 获取失败！" && exit 1
   tspcurrent=$(/usr/local/bin/tls-shunt-proxy 2>&1 | awk 'NR==1 {print $3}')
   if [ "${tsplatest}" == "${tspcurrent}" ]; then
     echo -e "[${green}Info${plain}] ${yellow}tls-shunt-proxy${plain}已安装最新版本${green}${tsplatest}${plain}。"
   else
+    mkdir "$TMP_DIR"
+    cd "$TMP_DIR" || exit 1
     echo -e "[${green}Info${plain}] 正在更新${yellow}tls-shunt-proxy${plain}：${red}${tspcurrent}${plain} --> ${green}${tsplatest}${plain}"
     wget -c "https://api.azzb.workers.dev/$tsplink"
     unzip -oq "tls-shunt-proxy-linux-amd64.zip"
     install -m 755 "tls-shunt-proxy" /usr/local/bin/
     systemctl restart tls-shunt-proxy
+    rm -rf "$TMP_DIR"
     echo -e "[${green}Info${plain}] ${yellow}tls-shunt-proxy${plain}更新成功！"
   fi
-  rm -rf "$TMP_DIR"
   exit 0
 }
 
@@ -166,8 +167,8 @@ vmess_change() {
 
 set_h2c() {
   port=2001
-  tls=tls
-  type=tcp
+  tls=none
+  type=h2
   echo -e -n "[${green}Info${plain}] 输入域名： "
   read -r domain
   echo -e -n "[${green}Info${plain}] 输入path： "
@@ -211,6 +212,22 @@ install_v2ray() {
   fi
   chore
   set_bbr
+}
+
+info_v2ray() {
+  status=$(pgrep -a v2ray | grep -c v2ray)
+  [ ! -f /etc/v2ray/config.json ] && echo -e "[${red}Error${plain}] 未找到V2Ray配置文件！" && exit 1
+  [ "$status" -eq 0 ] && v2status="${red}已停止${plain}" || v2status="${green}正在运行${plain}"
+  echo -e " id： ${green}$(grep < '/etc/v2ray/config.json' id | cut -d'"' -f4)${plain}"
+  echo -e " v2ray运行状态：${v2status}"
+}
+
+uninstall_v2ray() {
+  echo -e "[${green}Info${plain}] 正在卸载{yellow}tls-shunt-proxy${plain}..."
+  systemctl disable tls-shunt-proxy --now
+  rm -f /etc/systemd/system/tls-shunt-proxy.service
+  rm -f /usr/local/bin/tls-shunt-proxy
+  echo -e "[${green}Info${plain}] 卸载成功！"
 }
 
 action=$1
