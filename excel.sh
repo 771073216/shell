6355ea53cd465b2c6e022dc6bcdfc5913d487cb7
771073216 <771073216@qq.com>
2021-02-08 03:24:52 -0800

    update

---
+++ excel.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+  num=$(wc -l < 111.txt )
+  for ((i = 1; i <= "$num"; i++)); do
+        a[$i]=$(sed -n "$i"P < 111.txt | tr ' ' '*'|bc)
+  done
+  echo "${a[*]}" | tr ' ' '+'|bc
