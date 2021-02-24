1fc56b940508b5f7bcc338591c49ec5aad5c9ccb
771073216 <771073216@qq.com>
2021-02-24 12:41:40 +0800

    update

--- v2log.sh
+++ v2log.sh
@@ -3,22 +3,19 @@ green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
 file=/var/log/xray/access.log
-list=$(grep < "$file" shadowsocks | grep accepted | awk -F'[ :]' '{print$5}' | sort | uniq -c | sort -r)
+list=$(grep < "$file" shadowsocks | grep -v udp: | grep accepted | awk -F'[ :]' '{print$5}' | sort | uniq -c | sort -r)
 rejected=$(grep < "$file" -c rejected)
 replyatk=$(grep < "$file" -c 'failed to read IV')
 cryptoerror=$(grep < "$file" -c 'failed to read address')
-#udp=$(grep < "$file" -c 'finished')
 month=$(date "+%Y-%m")
 data=$(vnstat -s | grep "$month" | awk '{print$5,$6}')
 todaydata=$(vnstat -s | grep "today" | awk '{print$5,$6}')
-#list2=$(grep < "$file" finished | awk -F'[]:]+' '{print$10}' | sort | uniq -c | sort -r)
 def() {
   echo -ne "${yellow}detected repeated attack:$replyatk${plain}   "
   echo -e "${yellow}AEAD decrypt failed:$cryptoerror${plain}"
   echo -e "${yellow}connects rejected:$rejected${plain}"
   echo -e "${green}today TX data:$todaydata${plain}"
   echo -e "${green}month TX data:$data${plain}"
-  #  echo -e "${green}udp connection:$udp${plain}"
   if [ -z "$list" ]; then
     echo "no connection"
   else
@@ -26,9 +23,6 @@ def() {
     echo -e "         ${green}tcp${plain}"
     echo " counts      ip"
     echo "$list       "
-    #   echo -e "         ${green}udp${plain}"
-    #   echo " counts      ip"
-    #  echo "$list2"
     echo "------------------------------"
   fi
   DATA=$(apidata "$1")
@@ -36,17 +30,18 @@ def() {
   echo "-----------------------------"
 }
 
-#!/bin/bash
 apidata() {
-  xray api statsquery --server=127.0.0.1:10085 -pattern "" |
+  xray api statsquery --server=127.0.0.1:10085 |
     awk '{
         if (match($1, /name/)) {
-            f=1; gsub(/^"|link",$/, "", $2);
+            f=1; gsub(/"|link",$/, "", $2);
+            gsub(/"|link"$/, "", $2);
             split($2, p,  ">>>");
             printf "%s->%s\t", p[2],p[4];
         }
         else if (match($1, /value/) && f){ f = 0; printf "%.0f\n", $2; }
-        else if (match($0, /^>$/) && f) { f = 0; print 0; }
+        else if (match($0, /^{|}$/) && f) { f = 0; print 0; }
+        else if (match($0, /^{|},$/) && f) { f = 0; print 0; }
     }'
 }
 print_sum() {
@@ -101,6 +96,17 @@ b() {
   b
 }
 
+w() {
+  echo "$list"
+  echo -n "ip:"
+  read -r ip
+  if [ -z "$ip" ]; then
+    exit 0
+  fi
+  tcp=$(echo "$list" | head -n "$ip" | tail -n 1 | awk '{print$2}')
+  grep < "$file" "$tcp" | grep -v udp: | awk -F':' '{print$5}' | sort | uniq -c | sort -n
+}
+
 t() {
   tail -f "$file"
 }
@@ -108,7 +114,7 @@ t() {
 action=$1
 [ -z "$1" ] && action=def
 case "$action" in
-  def | q | l | b | t)
+  def | q | l | b | t | w)
     $action
     ;;
   *) ;;
