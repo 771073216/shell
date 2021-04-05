b7f1156cba0f73daceb2cfd437969805e1fd0d29
771073216 <37037387+771073216@users.noreply.github.com>
2021-04-06 00:36:53 +0800

    Update xray-h2.sh

--- xray-h2.sh
+++ xray-h2.sh
@@ -289,10 +289,10 @@ EOF
   [ "$caddystatus" -eq 0 ] && caddystatus="${r}已停止${p}" || caddystatus="${g}正在运行${p}"
   echo
   echo -e " ${y}(延迟更低~180ms)${p} 分享码1："
-  echo -e " ${r}vless://${h2uuid}@${domain}:443?encryption=none&security=tls&type=http&host=${domain}&path=${h2path}${p}"
+  echo -e " ${r}vless://${h2uuid}@${domain}:443?security=tls&type=http&host=${domain}&path=${h2path}${p}"
   echo
   echo -e " ${y}(延迟最低~90ms)[需要最新版v2rayN和v2rayNG]${p} 分享码2："
-  echo -e " ${r}vless://${grpcuuid}@${domain}:443?encryption=none&security=tls&type=grpc${p}"
+  echo -e " ${r}vless://${grpcuuid}@${domain}:443?security=tls&type=grpc&path=grpc#grpc${p}"
   echo
   echo -e " ${y}(ios专用~360ms)${p} 分享码3："
   echo -e " ${r}vmess://$(echo "$vmlink" | base64 | tr -d '\n')${p}"
