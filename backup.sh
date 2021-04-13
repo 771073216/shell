2f48121a14ac973edc2a96907777e63f71a06081
771073216 <37037387+771073216@users.noreply.github.com>
2021-04-13 10:39:57 +0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -7,13 +7,19 @@ b() {
   cp -r /etc/smartdns/ backup/
   cp -r /etc/caddy/ backup/
   cp -r /usr/local/etc/xray/ backup/
+  cp AdGuardHome/AdGuardHome.yaml backup/
   zip -r backup.zip backup/
 }
 
 r() {
+  https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_amd64.tar.gz
+  tar xvf AdGuardHome_linux_amd64.tar.gz
+  rm AdGuardHome_linux_amd64.tar.gz
+  apt install smartdns -y
   unzip backup.zip
   cp -r backup/www/ /var/
   mv backup/sshd_config /etc/ssh/
+  mv backup/AdGuardHome.yaml AdGuardHome/
   cp -r backup/.ssh/ .
   cp -r backup/smartdns/ /etc/
   cp -r backup/caddy/ /etc/
@@ -23,7 +29,7 @@ r() {
 }
 
 action=$1
-[ -z "$1" ] && exit 1
+[ -z "$1" ] && echo "b|r" && exit 0
 case "$action" in
   b | r)
     $action
