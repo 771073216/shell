a2aab57b5e5bd21c615776c332491ac0764c8076
771073216 <37037387+771073216@users.noreply.github.com>
2021-04-13 09:28:56 +0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,14 +4,20 @@ b() {
   cp -r /var/www/ backup/
   cp /etc/ssh/sshd_config backup/
   cp -r .ssh/ backup/
+  cp -r /etc/smartdns/ backup/
+  cp -r /etc/caddy/ backup/
+  cp -r /usr/local/etc/xray/ backup/
   zip -r backup.zip backup/
 }
 
 r() {
   unzip backup.zip
-  mv backup/www/ /var/
+  cp -r backup/www/ /var/
   mv backup/sshd_config /etc/ssh/
-  mv backup/.ssh/ .
+  cp -r backup/.ssh/ .
+  cp -r backup/smartdns/ /etc/
+  cp -r backup/caddy/ /etc/
+  cp -r backup/xray/ /usr/local/etc/
   rm -r backup/
   rm backup.zip
 }
