55346826d1f80016c5cf8874d9444a21856ba3bc
771073216 <37037387+771073216@users.noreply.github.com>
2021-05-11 01:04:57 +0800

    Update tbs.sh

--- tbs.sh
+++ tbs.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
-cd /data/data || exit
-rm -r ./*/app_tbs/*
-rm -r ./*/app_tbs_64/*
-chattr -i ./*/app_tbs
-chattr -i ./*/app_tbs_64
-chmod 000 ./*/app_tbs
-chmod 000 ./*/app_tbs_64
+list1=$(find /data/data/ -name app_tbs)
+num1=0
+for file in $list1
+do
+if [ -f "$file"/* ]; then
+  rm -r "$file"/*
+  chattr -i "$file"
+  chmod 000 "$file"
+  num1=$((num1 + 1))
+  echo "delete ${file}"
+fi
+done
+list2=$(find /data/data/ -name app_tbs_64)
+num2=0
+for file in $list2
+do
+if [ -f "$file"/* ]; then
+  rm -r "$file"/*
+  chattr -i "$file"
+  chmod 000 "$file"
+  num2=$((num2 + 1))
+  echo "delete ${file}"
+fi
+done
+sum=$((num1+num2))
+if [ $sum -eq 0 ]; then
+echo "everything is clean"
+fi
+if [ $num1 -ne 0 ]; then
+echo "clean $num1 app_tbs"
+fi
+if [ $num2 -ne 0 ]; then
+echo "clean $num2 app_tbs_64"
+fi
