54bf3f793c3d9b2ca982a45f442729141e1ce491
771073216 <771073216@qq.com>
2021-05-12 16:48:50 +0800

    update

--- tbs.sh
+++ tbs.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
-list1=$(find /data/data/ -name app_tbs)
 num1=0
-for file in $list1
-do
-if [ -f "$file"/* ]; then
-  rm -r "$file"/*
-  chattr -i "$file"
-  chmod 000 "$file"
-  num1=$((num1 + 1))
-  echo "delete ${file}"
-fi
-done
-list2=$(find /data/data/ -name app_tbs_64)
 num2=0
-for file in $list2
-do
-if [ -f "$file"/* ]; then
-  rm -r "$file"/*
-  chattr -i "$file"
-  chmod 000 "$file"
-  num2=$((num2 + 1))
-  echo "delete ${file}"
-fi
+list1=$(find /data/data/ -name app_tbs)
+list2=$(find /data/data/ -name app_tbs_64)
+for file in $list1; do
+  if find "${file}/*"; then
+    rm -r "${file:?}"/*
+    chattr -i "$file"
+    chmod 000 "$file"
+    num1=$((num1 + 1))
+    echo "delete $file"
+  fi
 done
-sum=$((num1+num2))
+for file in $list2; do
+  if find "${file}/*"; then
+    rm -r "${file:?}"/*
+    chattr -i "$file"
+    chmod 000 "$file"
+    num2=$((num2 + 1))
+    echo "delete $file"
+  fi
+done
+sum=$((num1 + num2))
 if [ $sum -eq 0 ]; then
-echo "everything is clean"
+  echo "everything is clean"
 fi
 if [ $num1 -ne 0 ]; then
-echo "clean $num1 app_tbs"
+  echo "clean $num1 app_tbs"
 fi
 if [ $num2 -ne 0 ]; then
-echo "clean $num2 app_tbs_64"
+  echo "clean $num2 app_tbs_64"
 fi
