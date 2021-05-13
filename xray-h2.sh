65a7578bd0fe80e0f05055c2a35691e7b7c9847c
771073216 <771073216@qq.com>
2021-05-13 23:07:03 +0800

    update

--- xray-h2.sh
+++ xray-h2.sh
@@ -4,11 +4,7 @@ g='\033[0;32m'
 y='\033[0;33m'
 p='\033[0m'
 TMP_DIR="$(mktemp -du)"
-h2conf=/usr/local/etc/xray/h2.yaml
 grpcconf=/usr/local/etc/xray/grpc.yaml
-link=https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
-link1=https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
-link2=https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
 
 [[ $EUID -ne 0 ]] && echo -e "[${r}Error${p}] 请以root身份执行该脚本！" && exit 1
 
@@ -24,29 +20,10 @@ pre_install() {
 }
 
 set_xray() {
-  h2uuid=$(cat /proc/sys/kernel/random/uuid)
-  cat > $h2conf <<- EOF
-inbounds:
-- port: 2001
-  listen: 127.0.0.1
-  protocol: vless
-  tag: h2
-  settings:
-    clients:
-    - id: "${h2uuid}"
-    decryption: none
-  streamSettings:
-    security: none
-    network: h2
-    httpSettings:
-      path: "/vlh2"
-      host:
-      - "${domain}"
-EOF
   grpcuuid=$(cat /proc/sys/kernel/random/uuid)
   cat > $grpcconf <<- EOF
 inbounds:
-- port: 2002
+- port: 2001
   listen: 127.0.0.1
   protocol: vless
   tag: grpc
@@ -63,17 +40,6 @@ EOF
 outbounds:
 - tag: direct
   protocol: freedom
-- tag: blocked
-  protocol: blackhole
-EOF
-  cat > /usr/local/etc/xray/routing.yaml <<- EOF
-routing:
-  domainStrategy: AsIs
-  rules:
-  - type: field
-    ip:
-    - geoip:private
-    outboundTag: blocked
 EOF
 }
 
@@ -81,8 +47,7 @@ set_caddy() {
   cat > /etc/caddy/Caddyfile <<- EOF
 ${domain} {
     @grpc protocol grpc
-    reverse_proxy /vlh2 h2c://127.0.0.1:2001
-    reverse_proxy @grpc h2c://127.0.0.1:2002
+    reverse_proxy @grpc h2c://127.0.0.1:2001
     root * /var/www
     file_server
 }
@@ -124,35 +89,27 @@ set_bbr() {
 install_caddy() {
   mkdir "$TMP_DIR"
   cd "$TMP_DIR" || exit 1
-  caddy_remote=$(wget -qO- "https://api.github.com/repos/caddyserver/caddy/releases/latest" | awk -F '"' '/tag_name/ {print $4}')
-  caddy_ver=$(echo "$caddy_remote" | tr -d v)
-  wget -q --show-progress https://github.com/caddyserver/caddy/releases/download/"$caddy_remote"/caddy_"$caddy_ver"_linux_amd64.deb
-  dpkg -i caddy_"$caddy_ver"_linux_amd64.deb
+  wget -q --show-progress https://cdn.jsdelivr.net/gh/771073216/dist@main/caddy.deb
+  dpkg -i caddy.deb
   rm -rf "$TMP_DIR"
 }
 
 install_file() {
   mkdir "$TMP_DIR"
   cd "$TMP_DIR" || exit 1
-  wget -q --show-progress "$link"
-  unzip -oq "Xray-linux-64.zip" xray -d /usr/local/bin/
+  wget -q --show-progress https://cdn.jsdelivr.net/gh/771073216/dist@main/xray-linux.zip
+  unzip -oq "xray-linux.zip" xray -d /usr/local/bin/
   rm -rf "$TMP_DIR"
 }
 
-install_geo() {
-  wget -q --show-progress "$link1" -O /usr/local/share/xray/geoip.dat
-  wget -q --show-progress "$link2" -O /usr/local/share/xray/geosite.dat
-}
-
 update_xray() {
-  xray_remote=$(wget -qO- "https://api.github.com/repos/XTLS/Xray-core/releases/latest" | awk -F '"' '/tag_name/ {print $4}')
-  xray_local=v$(/usr/local/bin/xray -version | awk 'NR==1 {print $2}')
-  remote_num=$(echo "$xray_remote" | tr -d .v)
-  local_num=$(echo "$xray_local" | tr -d .v)
+  xray_remote=$(wget -qO- "https://cdn.jsdelivr.net/gh/771073216/dist@main/version" | awk '/xray/ {print$2}')
+  xray_local=$(/usr/local/bin/xray -version | awk 'NR==1 {print $2}')
+  remote_num=$(echo "$xray_remote" | tr -d .)
+  local_num=$(echo "$xray_local" | tr -d .)
   if [ "${local_num}" -lt "${remote_num}" ]; then
-    echo -e "[${g}Info${p}] 正在更新${y}xray${p}：${r}${xray_local}${p} --> ${g}${xray_remote}${p}"
+    echo -e "[${g}Info${p}] 正在更新${y}xray${p}：${r}v${xray_local}${p} --> ${g}v${xray_remote}${p}"
     install_file
-    install_geo
     systemctl restart xray
     echo -e "[${g}Info${p}] ${y}xray${p}更新成功！"
   fi
@@ -160,16 +117,11 @@ update_xray() {
 }
 
 update_caddy() {
-  caddy_remote=$(wget -qO- "https://api.github.com/repos/caddyserver/caddy/releases/latest" | awk -F '"' '/tag_name/ {print $4}')
-  caddy_ver=$(echo "$caddy_remote" | tr -d v)
-  caddy_local=$(/usr/bin/caddy version | awk '{print$1}')
+  caddy_remote=$(wget -qO- "https://cdn.jsdelivr.net/gh/771073216/dist@main/version" | awk '/caddy/ {print$2}')
+  caddy_local=$(/usr/bin/caddy version | awk '{print$1}' | tr -d v)
   if ! [ "${caddy_local}" == "${caddy_remote}" ]; then
-    echo -e "[${g}Info${p}] 正在更新${y}caddy${p}：${r}${caddy_local}${p} --> ${g}${caddy_remote}${p}"
-    mkdir "$TMP_DIR"
-    cd "$TMP_DIR" || exit 1
-    wget -q --show-progress https://github.com/caddyserver/caddy/releases/download/"$caddy_remote"/caddy_"$caddy_ver"_linux_amd64.deb
-    dpkg -i caddy_"$caddy_ver"_linux_amd64.deb
-    rm -rf "$TMP_DIR"
+    echo -e "[${g}Info${p}] 正在更新${y}caddy${p}：${r}v${caddy_local}${p} --> ${g}v${caddy_remote}${p}"
+    install_caddy
     echo -e "[${g}Info${p}] ${y}caddy${p}更新成功！"
   fi
   if [ "${local_num}" -gt "${remote_num}" ]; then
@@ -189,7 +141,6 @@ install_xray() {
   set_service
   set_xray
   install_file
-  install_geo
   systemctl enable xray --now
   set_bbr
   info_xray
@@ -205,24 +156,18 @@ uninstall_xray() {
 }
 
 info_xray() {
-  h2uuid=$(awk -F'"' '/id:/ {print$2}' $h2conf)
   grpcuuid=$(awk -F'"' '/id:/ {print$2}' $grpcconf)
-  h2path=$(awk -F'"' '/path:/ {print$2}' $h2conf | tr -d /)
-  domain=$(awk 'NR==1 {print$1}' /etc/caddy/Caddyfile)
   xraystatus=$(pgrep -a xray | grep -c xray)
   caddystatus=$(pgrep -a caddy | grep -c caddy)
   echo
   [ "$xraystatus" -eq 0 ] && echo -e " xray运行状态：${r}已停止${p}" || echo -e " xray运行状态：${g}正在运行${p}"
   [ "$caddystatus" -eq 0 ] && echo -e " caddy运行状态：${r}已停止${p}" || echo -e " caddy运行状态：${g}正在运行${p}"
   echo
-  echo -e " ${y}(延迟更低~180ms)${p} 分享码1："
-  echo -e " ${r}vless://${h2uuid}@${domain}:443?type=http&encryption=none&security=tls&path=${h2path}&host=${domain}#h2${p}"
-  echo
-  echo -e " ${y}(延迟最低~90ms)[需要最新版v2rayN和v2rayNG]${p} 分享码2："
+  echo -e " ${y}[需要最新版v2rayN和v2rayNG]${p} 分享码："
   echo -e " ${r}vless://${grpcuuid}@${domain}:443?type=grpc&encryption=none&security=tls&serviceName=grpc#grpc${p}"
   echo
-  echo -e "(windows)v2rayN下载链接：${g}https://github.com/2dust/v2rayN/releases/download/4.13/v2rayN.zip${p}"
-  echo -e "(android)v2rayNG下载链接：${g}https://github.com/2dust/v2rayNG/releases/download/1.6.3/v2rayNG_1.6.3_arm64-v8a.apk${p}"
+  echo -e "(windows)v2rayN下载链接：${g}https://cdn.jsdelivr.net/gh/771073216/dist@main/v2rayn-core.zip${p}"
+  echo -e "(android)v2rayNG下载链接：${g}https://cdn.jsdelivr.net/gh/771073216/dist@main/v2rayng.apk${p}"
 }
 
 manual() {
@@ -231,8 +176,11 @@ manual() {
   echo "correct?  q = quit "
   read -r co
   if ! [ "$co" = q ]; then
-    link=https://github.com/XTLS/Xray-core/releases/download/$ver/Xray-linux-64.zip
-    install_file
+    mkdir "$TMP_DIR"
+    cd "$TMP_DIR" || exit 1
+    wget -q --show-progress https://github.com/XTLS/Xray-core/releases/download/"$ver"/Xray-linux-64.zip
+    unzip -oq "Xray-linux-64.zip" xray -d /usr/local/bin/
+    rm -rf "$TMP_DIR"
     systemctl restart xray
   else
     echo "cancel"
