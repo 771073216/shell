d5476ae8346357bf2b03d05d1a6576fa157ea2ed
771073216 <771073216@qq.com>
2021-05-16 23:02:13 +0800

    up

--- backup.sh
+++ backup.sh
@@ -4,32 +4,24 @@ b() {
   cp -r /var/www/ backup/
   cp /etc/ssh/sshd_config backup/
   cp -r .ssh/ backup/
-  cp -r /etc/smartdns/ backup/
   cp -r /etc/caddy/ backup/
   cp -r /usr/local/etc/xray/ backup/
-  cp AdGuardHome/AdGuardHome.yaml backup/
   zip -r backup.zip backup/
+  rm -r backup/
 }
 
 r() {
-  https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_amd64.tar.gz
-  tar xvf AdGuardHome_linux_amd64.tar.gz
-  ./AdGuardHome/AdGuardHome -s install
-  rm AdGuardHome_linux_amd64.tar.gz
-  apt install smartdns -y
   unzip backup.zip
   cp -r backup/www/ /var/
   mv backup/sshd_config /etc/ssh/
-  mv backup/AdGuardHome.yaml AdGuardHome/
   cp -r backup/.ssh/ .
-  cp -r backup/smartdns/ /etc/
   cp -r backup/caddy/ /etc/
   cp -r backup/xray/ /usr/local/etc/
   mkdir /var/log/xray
   chown -R nobody:nogroup /var/log/xray
   rm -r backup/
   rm backup.zip
-  systemctl restart smartdns AdGuardHome xray caddy sshd
+  systemctl restart xray caddy sshd
 }
 
 action=$1
