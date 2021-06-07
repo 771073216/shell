3207e853c53aa332df798cce5841a47c0d266cb5
771073216 <771073216@qq.com>
2021-06-07 21:12:57 +0800

    update

--- xray-h2.sh
+++ xray-h2.sh
@@ -3,22 +3,19 @@ r='\033[0;31m'
 g='\033[0;32m'
 y='\033[0;33m'
 p='\033[0m'
-TMP_DIR="$(mktemp -du)"
 
 [[ $EUID -ne 0 ]] && echo -e "[${r}Error${p}] 请以root身份执行该脚本！" && exit 1
+rm -rf /tmp/xray && mkdir /tmp/xray
 
 install_xray() {
   uuid=$(cat /proc/sys/kernel/random/uuid)
   [ -f /usr/local/bin/xray ] && update_xray
   echo -e -n "[${g}Info${p}] 输入域名： "
   read -r domain
-  mkdir "$TMP_DIR"
-  cd "$TMP_DIR" || exit 1
-  wget -q --show-progress https://cdn.jsdelivr.net/gh/771073216/dist@main/xray.deb
-  dpkg -i xray.deb
+  wget -q --show-progress https://cdn.jsdelivr.net/gh/771073216/dist@main/xray.deb -O /tmp/xray/xray.deb
+  dpkg -i /tmp/xray/xray.deb && rm -rf /tmp/xray
   sed -i "s/uuid/$uuid/g" /usr/local/etc/xray/config.yaml
   sed -i "1c$domain {" /usr/local/etc/caddy/Caddyfile
-  rm -rf "$TMP_DIR"
   systemctl restart xray caddy
   echo -e "[${g}Info${p}] 设置bbr..."
   sed -i '/net.core.default_qdisc/d' '/etc/sysctl.conf'
@@ -30,16 +27,13 @@ install_xray() {
 }
 
 update_xray() {
-  mkdir "$TMP_DIR"
-  cd "$TMP_DIR" || exit 1
-  wget -q "https://cdn.jsdelivr.net/gh/771073216/dist@main/version"
-  xray_remote=$(awk '/xray/ {print$2}' version)
-  caddy_remote=$(awk '/caddy/ {print$2}' version)
+  wget -q "https://cdn.jsdelivr.net/gh/771073216/dist@main/version" -O /tmp/xray/verison
+  xray_remote=$(awk '/xray/ {print$2}' /tmp/xray/version)
+  caddy_remote=$(awk '/caddy/ {print$2}' /tmp/xray/version)
   xray_local=$(/usr/local/bin/xray -version | awk 'NR==1 {print $2}')
   caddy_local=$(/usr/bin/caddy version | awk '{print$1}' | tr -d v)
   remote_num=$(echo "$xray_remote" | tr -d .)
   local_num=$(echo "$xray_local" | tr -d .)
-  echo "========================="
   if [ "${local_num}" -lt "${remote_num}" ]; then
     echo -e "| ${y}xray${p}  | ${r}v${xray_local}${p} --> ${g}v${xray_remote}${p}"
     update="1"
@@ -52,13 +46,11 @@ update_xray() {
   else
     echo -e "| ${y}caddy${p} | ${g}v${caddy_local}${p}"
   fi
-  echo "========================="
   if [ "${update}" -eq 1 ]; then
-    wget -q --show-progress https://cdn.jsdelivr.net/gh/771073216/dist@main/xray.deb
-    dpkg -i xray.deb
+    wget -q --show-progress https://cdn.jsdelivr.net/gh/771073216/dist@main/xray.deb -O /tmp/xray/xray.deb
+    dpkg -i /tmp/xray/xray.deb && rm -rf /tmp/xray
     echo -e "[${g}Info${p}] 更新成功！"
   fi
-  rm -rf "$TMP_DIR"
   exit 0
 }
 
@@ -86,12 +78,9 @@ info_xray() {
 manual() {
   ver=$(wget -qO- https://api.github.com/repos/XTLS/Xray-core/tags | awk -F '"' '/name/ {print $4}' | head -n 1)
   xray_local=$(/usr/local/bin/xray -version | awk 'NR==1 {print $2}')
-  mkdir "$TMP_DIR"
-  cd "$TMP_DIR" || exit 1
   echo -e "[${g}Info${p}] 正在更新${y}xray${p}：${r}v${xray_local}${p} --> ${g}${ver}${p}"
-  wget -q --show-progress https://github.com/XTLS/Xray-core/releases/download/"$ver"/Xray-linux-64.zip
-  unzip -oq "Xray-linux-64.zip" xray -d /usr/local/bin/
-  rm -rf "$TMP_DIR"
+  wget -q --show-progress https://github.com/XTLS/Xray-core/releases/download/"$ver"/Xray-linux-64.zip -O /tmp/xray/xray.zip
+  unzip -oq "/tmp/xray/xray.zip" xray -d /usr/local/bin/ && rm -rf /tmp/xray
   systemctl restart xray
   echo -e "[${g}Info${p}] ${y}xray${p}更新成功！"
 }
