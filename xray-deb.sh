0a948c41fcae884142cdd8ca3f1f465cba5cb18f
771073216 <771073216@qq.com>
2021-06-15 21:56:36 +0800

    update

---
+++ xray-deb.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+r='\033[0;31m'
+g='\033[0;32m'
+y='\033[0;33m'
+p='\033[0m'
+
+[[ $EUID -ne 0 ]] && echo -e "[${r}Error${p}] 请以root身份执行该脚本！" && exit 1
+rm -rf /tmp/xray && mkdir /tmp/xray
+
+install_xray() {
+  uuid=$(cat /proc/sys/kernel/random/uuid)
+  if ! dpkg -l | grep xray; then
+    update_xray
+  fi
+  echo -e -n "[${g}Info${p}] 输入域名： "
+  read -r domain
+  wget -q --show-progress https://cdn.jsdelivr.net/gh/771073216/dist@main/xray.deb -O /tmp/xray/xray.deb
+  dpkg -i /tmp/xray/xray.deb && rm -rf /tmp/xray
+  sed -i "s/uuid/$uuid/g" /usr/local/etc/xray/config.yaml
+  sed -i "1c$domain {" /usr/local/etc/caddy/Caddyfile
+  systemctl restart xray caddy
+  info_xray
+}
+
+update_xray() {
+  remote_version=$(wget -qO- "https://cdn.jsdelivr.net/gh/771073216/dist@main/version" | awk '/xray+caddy/ {print$2}')
+  local_version=$(dpkg -s xray | awk '/Version/ {print$2}')
+  if ! [ "${remote_version}" == "${local_version}" ]; then
+    echo -e "| ${y}xray+caddy${p}  | ${r}${local_version}${p} --> ${g}${remote_version}${p}"
+    wget -q --show-progress https://cdn.jsdelivr.net/gh/771073216/dist@main/xray.deb -O /tmp/xray/xray.deb
+    dpkg -i /tmp/xray/xray.deb && rm -rf /tmp/xray
+    echo -e "[${g}Info${p}] 更新成功！"
+  else
+    echo -e "| ${y}xray+caddy${p}  | ${g}${local_version}${p}"
+  fi
+  exit 0
+}
+
+uninstall_xray() {
+  echo -e "[${g}Info${p}] 正在卸载${y}xray${p}..."
+  dpkg --purge xray
+  echo -e "[${g}Info${p}] 卸载成功！"
+}
+
+info_xray() {
+  uuid=$(awk -F'"' '/id:/ {print$2}' /usr/local/etc/xray/config.yaml)
+  domain=$(awk 'NR==1 {print$1}' /usr/local/etc/caddy/Caddyfile)
+  xraystatus=$(pgrep -a xray | grep -c xray)
+  caddystatus=$(pgrep -a caddy | grep -c caddy)
+  echo
+  [ "$xraystatus" -eq 0 ] && echo -e " xray运行状态：${r}已停止${p}" || echo -e " xray运行状态：${g}正在运行${p}"
+  [ "$caddystatus" -eq 0 ] && echo -e " caddy运行状态：${r}已停止${p}" || echo -e " caddy运行状态：${g}正在运行${p}"
+  echo
+  echo -e " 分享码："
+  echo -e " ${r}vless://${uuid}@${domain}:443?type=grpc&encryption=none&security=tls&serviceName=grpc#grpc${p}"
+  echo
+  echo -e "(windows)v2rayN下载链接：${g}https://cdn.jsdelivr.net/gh/771073216/dist@main/v2rayn-core.zip${p}"
+  echo -e "(android)v2rayNG下载链接：${g}https://cdn.jsdelivr.net/gh/771073216/dist@main/v2rayng.apk${p}"
+}
+
+action=$1
+[ -z "$1" ] && action=install
+case "$action" in
+  install | info | uninstall)
+    ${action}_xray
+    ;;
+  *)
+    echo "参数错误！ [${action}]"
+    echo "使用方法：$(basename "$0") [install|uninstall|info|-m]"
+    ;;
+esac
--- xray-h2.sh
+++ xray-h2.sh
@@ -3,59 +3,88 @@ r='\033[0;31m'
 g='\033[0;32m'
 y='\033[0;33m'
 p='\033[0m'
+TMP_DIR="$(mktemp -du)"
 
 [[ $EUID -ne 0 ]] && echo -e "[${r}Error${p}] 请以root身份执行该脚本！" && exit 1
-rm -rf /tmp/xray && mkdir /tmp/xray
 
 install_xray() {
   uuid=$(cat /proc/sys/kernel/random/uuid)
   [ -f /usr/local/bin/xray ] && update_xray
   echo -e -n "[${g}Info${p}] 输入域名： "
   read -r domain
-  wget -q --show-progress https://cdn.jsdelivr.net/gh/771073216/dist@main/xray.deb -O /tmp/xray/xray.deb
-  dpkg -i /tmp/xray/xray.deb && rm -rf /tmp/xray
-  sed -i "s/uuid/$uuid/g" /usr/local/etc/xray/config.yaml
-  sed -i "1c$domain {" /usr/local/etc/caddy/Caddyfile
-  systemctl restart xray caddy
+  if ! command -v "unzip" > /dev/null 2>&1; then
+    echo -e "[${g}Info${p}] 正在安装${y}unzip${p}..."
+    apt install unzip -y > /dev/null 2>&1
+  fi
+  mkdir -p /usr/local/etc/xray/ /etc/caddy/ /var/www/
+  mkdir "$TMP_DIR"
+  cd "$TMP_DIR" || exit 1
+  wget -q --show-progress https://cdn.jsdelivr.net/gh/771073216/dist@main/xray-inst.zip
+  unzip -q "xray-inst.zip"
+  sed -i "s/uuid/$uuid/g" config.yaml
+  sed -i "s/domain/$domain/g" Caddyfile
+  dpkg -i caddy.deb
+  mv xray /usr/local/bin/
+  mv xray.service /etc/systemd/system/
+  mv config.yaml /usr/local/etc/xray/
+  mv Caddyfile /etc/caddy/
+  mv index.html /var/www/
+  rm -rf "$TMP_DIR"
+  systemctl enable xray --now
+  systemctl restart caddy
+  echo -e "[${g}Info${p}] 设置bbr..."
+  sed -i '/net.core.default_qdisc/d' '/etc/sysctl.conf'
+  sed -i '/net.ipv4.tcp_congestion_control/d' '/etc/sysctl.conf'
+  (echo "net.core.default_qdisc = fq" && echo "net.ipv4.tcp_congestion_control = bbr") >> '/etc/sysctl.conf'
+  sysctl -p > /dev/null 2>&1
   info_xray
 }
 
 update_xray() {
-  wget -q "https://cdn.jsdelivr.net/gh/771073216/dist@main/version" -O /tmp/xray/verison
-  xray_remote=$(awk '/xray/ {print$2}' /tmp/xray/version)
-  caddy_remote=$(awk '/caddy/ {print$2}' /tmp/xray/version)
+  mkdir "$TMP_DIR"
+  cd "$TMP_DIR" || exit 1
+  wget -q "https://cdn.jsdelivr.net/gh/771073216/dist@main/version"
+  xray_remote=$(awk '/xray/ {print$2}' version)
+  caddy_remote=$(awk '/caddy/ {print$2}' version)
   xray_local=$(/usr/local/bin/xray -version | awk 'NR==1 {print $2}')
   caddy_local=$(/usr/bin/caddy version | awk '{print$1}' | tr -d v)
   remote_num=$(echo "$xray_remote" | tr -d .)
   local_num=$(echo "$xray_local" | tr -d .)
   if [ "${local_num}" -lt "${remote_num}" ]; then
-    echo -e "| ${y}xray${p}  | ${r}v${xray_local}${p} --> ${g}v${xray_remote}${p}"
-    update="1"
-  else
-    echo -e "| ${y}xray${p}  | ${g}v${xray_local}${p}"
+    echo -e "[${g}Info${p}] 正在更新${y}xray${p}：${r}v${xray_local}${p} --> ${g}v${xray_remote}${p}"
+    wget -q --show-progress https://cdn.jsdelivr.net/gh/771073216/dist@main/xray-linux.zip
+    unzip -oq "xray-linux.zip" xray -d /usr/local/bin/
+    systemctl restart xray
+    echo -e "[${g}Info${p}] ${y}xray${p}更新成功！"
   fi
   if ! [ "${caddy_local}" == "${caddy_remote}" ]; then
-    echo -e "| ${y}caddy${p} | ${r}v${caddy_local}${p} --> ${g}v${caddy_remote}${p}"
-    update="1"
+    echo -e "[${g}Info${p}] 正在更新${y}caddy${p}：${r}v${caddy_local}${p} --> ${g}v${caddy_remote}${p}"
+    wget -q --show-progress https://cdn.jsdelivr.net/gh/771073216/dist@main/caddy.deb
+    dpkg -i caddy.deb
+    echo -e "[${g}Info${p}] ${y}caddy${p}更新成功！"
+  fi
+  if [ "${local_num}" -gt "${remote_num}" ]; then
+    echo -e "[${g}Info${p}] ${y}xray${p}已安装pre版本${g}${xray_local}${p}。"
   else
-    echo -e "| ${y}caddy${p} | ${g}v${caddy_local}${p}"
+    echo -e "[${g}Info${p}] ${y}xray${p}已安装最新版本${g}${xray_remote}${p}。"
   fi
-  if [ "${update}" -eq 1 ]; then
-    wget -q --show-progress https://cdn.jsdelivr.net/gh/771073216/dist@main/xray.deb -O /tmp/xray/xray.deb
-    dpkg -i /tmp/xray/xray.deb && rm -rf /tmp/xray
-    echo -e "[${g}Info${p}] 更新成功！"
-  fi
+  echo -e "[${g}Info${p}] ${y}caddy${p}已安装最新版本${g}${caddy_remote}${p}。"
+  rm -rf "$TMP_DIR"
   exit 0
 }
 
 uninstall_xray() {
   echo -e "[${g}Info${p}] 正在卸载${y}xray${p}..."
-  dpkg --purge xray
+  systemctl disable xray --now
+  rm -f /usr/local/bin/xray
+  rm -rf /usr/local/etc/xray/
+  rm -f /etc/systemd/system/xray.service
   echo -e "[${g}Info${p}] 卸载成功！"
 }
 
 info_xray() {
   uuid=$(awk -F'"' '/id:/ {print$2}' /usr/local/etc/xray/config.yaml)
+  domain=$(awk 'NR==1 {print$1}' /etc/caddy/Caddyfile)
   xraystatus=$(pgrep -a xray | grep -c xray)
   caddystatus=$(pgrep -a caddy | grep -c caddy)
   echo
@@ -72,9 +101,12 @@ info_xray() {
 manual() {
   ver=$(wget -qO- https://api.github.com/repos/XTLS/Xray-core/tags | awk -F '"' '/name/ {print $4}' | head -n 1)
   xray_local=$(/usr/local/bin/xray -version | awk 'NR==1 {print $2}')
+  mkdir "$TMP_DIR"
+  cd "$TMP_DIR" || exit 1
   echo -e "[${g}Info${p}] 正在更新${y}xray${p}：${r}v${xray_local}${p} --> ${g}${ver}${p}"
-  wget -q --show-progress https://github.com/XTLS/Xray-core/releases/download/"$ver"/Xray-linux-64.zip -O /tmp/xray/xray.zip
-  unzip -oq "/tmp/xray/xray.zip" xray -d /usr/local/bin/ && rm -rf /tmp/xray
+  wget -q --show-progress https://github.com/XTLS/Xray-core/releases/download/"$ver"/Xray-linux-64.zip
+  unzip -oq "Xray-linux-64.zip" xray -d /usr/local/bin/
+  rm -rf "$TMP_DIR"
   systemctl restart xray
   echo -e "[${g}Info${p}] ${y}xray${p}更新成功！"
 }
