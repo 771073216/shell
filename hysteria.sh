05b0902cc5c119088b6244d76d5222d17ae71c52
771073216 <771073216@qq.com>
2021-08-18 01:44:36 +0800

    update

--- hysteria.sh
+++ hysteria.sh
@@ -6,7 +6,7 @@ p='\033[0m'
 
 [[ $EUID -ne 0 ]] && echo -e "[${r}Error${p}] 请以root身份执行该脚本！" && exit 1
 
-check_port(){
+check_port() {
   port_status=$(lsof -i :80)
   [ -n "$port_status" ] && echo "port 80 is used" && exit 1
 }
--- ping.sh
+++ ping.sh
@@ -1,39 +1,41 @@
 #!/bin/sh
-main(){
-list="sp kr tk os"
-echo "testing"
-for p in $list; do
-{
-  $p
-  } &
-done
-wait
+main() {
+  list="sp kr tk os"
+  time=$(date "+%d%H")
+  ltime=$(date)
+  old=$(("$time" - 100))
+  rm -f $old.log
+  echo "" > /root/"$time".log
+  echo "$ltime" >> /root/"$time".log
+  for p in $list; do
+    {
+      $p
+    } &
+  done
+  wait
 }
 
-kr(){
-ip="13.124.63.251"
-test $ip "korea"
+kr() {
+  test "13.124.63.251" "korea"
 }
 
-tk(){
-ip="13.208.32.253"
-test $ip "tokyo"
+tk() {
+  test "13.208.32.253" "tokyo"
 }
 
-os(){
-ip="18.182.0.0"
-test $ip "osaka"
+os() {
+  test "18.182.0.0" "osaka"
 }
 
-sp(){
-test "13.250.0.253" "singapore"
+sp() {
+  test "13.250.0.253" "singapore"
 }
 
-test(){
-a=$(ping -c 200 -W 1 $1 | tail -n2)
-loss=$(echo $a | awk '/loss/ {print$7}')
-round=$(echo $a | awk '{print$11" "$13$14}')
-echo "aws $2: loss $loss   $round"
+test() {
+  a=$(ping -c 200 -W 1 "$1" | tail -n2)
+  loss=$(echo "$a" | awk '/loss/ {print$7}')
+  round=$(echo "$a" | awk '{print$11" "$13$14}')
+  printf "%-15s%-10s%-20s\n" "$2:" "loss $loss" "$round" >> /root/"$time".log
 }
 
 main
