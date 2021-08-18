40efd5f11ee23c037ce93a20bf38593f250b3898
771073216 <771073216@qq.com>
2021-08-18 11:48:13 +0800

    update

---
+++ init.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+apt install bash-completion wget unzip
+var0=$(echo "\$(dircolors -b ~/.dircolors)")
+var1=$(echo "\$(dircolors -b)")
+cat > .bashrc <<- EOF
+if [ -x /usr/bin/dircolors ]; then
+  test -r ~/.dircolors && eval "$var0" || eval "$var1"
+  alias ls='ls --color=auto'
+    #alias dir='dir --color=auto'
+       #alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+        alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+EOF
+cat >> .profile <<- EOF
+if [ -f /etc/bash_completion ]; then
+. /etc/bash_completion
+fi
+EOF
+source .bashrc
+source .profile
