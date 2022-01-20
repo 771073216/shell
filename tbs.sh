b312245cb3d3249acaf949491e4f137574cb8160
771073216 <771073216@qq.com>
2022-01-20 17:49:36 +0800

    update

--- tbs.sh
+++ tbs.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 for dir in $(cmd package list package -3 | awk -F':' '{print$2}'); do
   if find /data/data/"$dir"/app_tbs > /dev/null 2>&1; then
-    rm -rf /data/data/"$dir"/app_tbs*/*
-    chattr -i /data/data/"$dir"/app_tbs*
-    chmod 000 /data/data/"$dir"/app_tbs*
-    echo "Clean $dir"
+    if [ -n "$(ls /data/data/"$dir"/app_tbs* | grep -v :)" ]; then
+      rm -rf /data/data/"$dir"/app_tbs*/*
+      chattr -i /data/data/"$dir"/app_tbs*
+      chmod 000 /data/data/"$dir"/app_tbs*
+      echo "Clean $dir..."
+    else
+      echo "Everything is clean."
+    fi
   fi
 done
