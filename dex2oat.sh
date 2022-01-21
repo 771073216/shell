575555da1a5b8849a85a837ef0b8ba79004418e3
771073216 <37037387+771073216@users.noreply.github.com>
2022-01-22 00:59:46 +0800

    Update dex2oat.sh

--- dex2oat.sh
+++ dex2oat.sh
@@ -3,14 +3,14 @@ i=0
 num=$(cmd package list package -a | awk -F':' '{print$2}' | grep -c "")
 for package in $(cmd package list package -a | awk -F':' '{print$2}'); do
   i=$((i + 1))
-  cmd package compile -m speed "$package" > /dev/null
   echo "$i/$num $package"
+  cmd package compile -m speed "$package" > /dev/null
 done
 
 i=0
 num=$(cmd package list package -a | awk -F':' '{print$2}' | grep -xv android | grep -c "")
 for package in $(cmd package list package -a | awk -F':' '{print$2}' | grep -xv android); do
   i=$((i + 1))
-  cmd package compile -m speed "$package" --secondary-dex > /dev/null
   echo "$i/$num $package"
+  cmd package compile -m speed "$package" --secondary-dex > /dev/null
 done
