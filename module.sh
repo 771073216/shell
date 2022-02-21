bd0cde63fbde1283efe45a7ecb259cc5895100e2
771073216 <771073216@qq.com>
2022-02-21 16:58:08 +0800

    update

--- module.sh
+++ module.sh
@@ -1,45 +1,48 @@
 #!/bin/sh
 get_info() {
   i=0
-  list=$(ls /data/adb/modules/)
+  list=$(find /data/adb/modules/ -mindepth 1 -maxdepth 1)
   for module in $list; do
-    m=$(awk -F"=" '/name/ {print$2}' /data/adb/modules/"$module"/module.prop)
+    m=$(awk -F"=" '/name/ {print$2}' "$module"/module.prop)
+    module_name=$(basename "$module")
     i=$((i + 1))
-    if [ -f /data/adb/modules/"$module"/disable ]; then
+    if [ -f "$module"/disable ]; then
       stat="disabled"
     else
       stat="enabled"
     fi
-    echo "[$i] $module ($m) [$stat]"
+    echo "[$i] $module_name ($m) [$stat]"
   done
 }
 
 enable_module() {
   echo
   echo "enable which $sum"
-  read -r i
   printf "input: "
+  read -r i
   [ -z "$i" ] && echo "echo number" && exit 1
-  name=$(ls /data/adb/modules/ | awk -v "i=$i" 'NR==i {print$1}')
-  if [ -f /data/adb/modules/"$name"/disable ]; then
-    rm /data/adb/modules/"$name"/disable
+  module=$(echo "$list" | awk -v "i=$i" 'NR==i {print$1}')
+  module_name=$(basename "$module")
+  if [ -f "$module"/disable ]; then
+    rm "$module"/disable
     echo "done"
   else
-    echo "$name is enabled"
+    echo "$module_name is enabled"
   fi
 }
 
 disable_module() {
   echo
   echo "disable which $sum"
-  read -r i
   printf "input: "
+  read -r i
   [ -z "$i" ] && echo "echo number" && exit 1
-  name=$(ls /data/adb/modules/ | awk -v "i=$i" 'NR==i {print$1}')
-  if [ -f /data/adb/modules/"$name"/disable ]; then
-    echo "$name is disabled"
+  module=$(echo "$list" | awk -v "i=$i" 'NR==i {print$1}')
+  module_name=$(basename "$module")
+  if [ -f "$module"/disable ]; then
+    echo "$module_name is disabled"
   else
-    touch /data/adb/modules/"$name"/disable
+    touch "$module"/disable
     echo "done"
   fi
 }
--- tbs.sh
+++ tbs.sh
@@ -2,7 +2,7 @@
 i=0
 for dir in $(cmd package list package -3 | awk -F':' '{print$2}'); do
   if find /data/data/"$dir"/app_tbs > /dev/null 2>&1; then
-    if [ -n "$(ls /data/data/"$dir"/app_tbs* | grep -v :)" ]; then
+    if [ -n "$(find /data/data/"$dir"/app_tbs* -mindepth 1 -name '*')" ]; then
       rm -rf /data/data/"$dir"/app_tbs*/*
       chattr -i /data/data/"$dir"/app_tbs*
       chmod 000 /data/data/"$dir"/app_tbs*
