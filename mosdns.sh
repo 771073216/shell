fca333ceb589a6283f2950530dedd29990adc10d
771073216 <771073216@qq.com>
2022-05-22 15:23:12 +0800

    update

--- mosdns.sh
+++ mosdns.sh
@@ -10,6 +10,13 @@ echo "[redis] install redis-server"
 printf "[input]: "
 read -r select
 
+arch=$(awk -F'"' '/OPENWRT_ARCH/{print$2}' /etc/os-release)
+if [ "$arch" = "x86_64" ]; then
+  mosdns_arch="amd64"
+else
+  mosdns_arch="arm64"
+fi
+
 if [ "$select" = 1 ]; then
   log_file=$(awk -F"'" '/log/&&/file/{print$2}' /etc/mosdns/config.yaml)
   echo "" > "$log_file"
@@ -22,22 +29,25 @@ if [ "$select" = 2 ]; then
 fi
 
 if [ "$select" = 3 ]; then
-  old=$(mosdns -v)
+  local_ver=$(mosdns -v | awk -F"-" '{print$1}')
+  remote_ver=$(curl -sSL https://api.github.com/repos/IrineSistiana/mosdns/releases/latest | awk -F'"' '/tag_name/{print$4}')
+  if [ "$local_ver" = "$remote_ver" ]; then
+    return
+  fi
   mkdir /tmp/mosdns-update
-  wget https://github.com/IrineSistiana/mosdns/releases/latest/download/mosdns-linux-arm64.zip -O /tmp/mosdns-update/mosdns.zip
+  curl -sSL https://github.com/IrineSistiana/mosdns/releases/latest/download/mosdns-linux-${mosdns_arch}.zip -o /tmp/mosdns-update/mosdns.zip
   unzip /tmp/mosdns-update/mosdns.zip mosdns -d /tmp/mosdns-update
   rm /usr/bin/mosdns
   mv /tmp/mosdns-update/mosdns /usr/bin/mosdns
   rm -r /tmp/mosdns-update
-  new=$(mosdns -v)
-  echo "$old -> $new"
+  echo "$local_ver -> $remote_ver"
   log_file=$(awk -F"'" '/log/&&/file/{print$2}' /etc/mosdns/config.yaml)
   echo "" > "$log_file"
   /etc/init.d/mosdns restart
 fi
 
 if [ "$select" = 4 ]; then
-  version=$(curl -sSL -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/771073216/geofile/releases/latest" | awk -F '"' '/tag_name/ {print $4}')
+  version=$(curl -sSL "https://api.github.com/repos/771073216/geofile/releases/latest" | awk -F '"' '/tag_name/ {print $4}')
   if [ -e /usr/share/v2ray/version ]; then
     local_ver=$(cat /usr/share/v2ray/version)
   else
@@ -73,7 +83,7 @@ fi
 
 if [ "$select" = "install" ]; then
   mkdir /tmp/mosdns-install /etc/mosdns
-  wget https://github.com/IrineSistiana/mosdns/releases/latest/download/mosdns-linux-arm64.zip -O /tmp/mosdns-install/mosdns.zip
+  wget https://github.com/IrineSistiana/mosdns/releases/latest/download/mosdns-linux-${mosdns_arch}.zip -O /tmp/mosdns-install/mosdns.zip
   unzip /tmp/mosdns-install/mosdns.zip mosdns -d /usr/bin/
   wget https://raw.githubusercontent.com/IrineSistiana/mosdns/main/scripts/openwrt/mosdns-init-openwrt -O /etc/init.d/mosdns
   chmod +x /etc/init.d/mosdns
@@ -83,7 +93,6 @@ fi
 
 if [ "$select" = "redis" ]; then
   mkdir /usr/share/redis
-  arch=$(awk -F'"' '/OPENWRT_ARCH/{print$2}' /etc/os-release)
   file=$(curl -s https://mirrors.cloud.tencent.com/lede/snapshots/packages/"$arch"/packages/ | awk -F'"' '/redis-server/ {print$2}')
   wget https://mirrors.cloud.tencent.com/lede/snapshots/packages/"$arch"/packages/"$file" -O /root/"$file"
 fi
