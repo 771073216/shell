624f7056534310176f6792e2b24056028a61295f
771073216 <771073216@qq.com>
2022-05-25 14:42:34 +0800

    update

--- vnstat.sh
+++ vnstat.sh
@@ -1,7 +1,30 @@
 #!/usr/bin/env bash
-apt install -y build-essential libsqlite3-dev git
-git clone https://github.com/vergoh/vnstat.git && cd vnstat || exit 1
-./configure --prefix=/usr --sysconfdir=/etc && make && make install
-cp examples/systemd/vnstat.service /etc/systemd/system/
-systemctl enable vnstat.service --now
-cd .. && rm -r vnstat
+remote_ver=$(curl -sSL https://api.github.com/repos/vergoh/vnstat/releases/latest | awk -F'"' '/tag_name/{print$4}')
+main() {
+  if command -v vnstat; then
+    update
+    return
+  fi
+  apt install -y build-essential libsqlite3-dev git
+  curl -L https://codeload.github.com/vergoh/vnstat/zip/refs/tags/"${remote_ver}" && cd vnstat || exit 1
+  ./configure --prefix=/usr --sysconfdir=/etc && make && make install
+  cp examples/systemd/vnstat.service /etc/systemd/system/
+  systemctl enable vnstat.service --now
+  cd .. && rm -r vnstat
+}
+
+update() {
+  local_ver=v$(vnstat -v | awk '{print$2}')
+  if [ "${local_ver}" == "${remote_ver}" ]; then
+    return
+  fi
+  [ -e vnstat ] && rm -r vnstat
+  curl -L https://codeload.github.com/vergoh/vnstat/zip/refs/tags/"${remote_ver}" && cd vnstat || exit 1
+  ./configure --prefix=/usr --sysconfdir=/etc && make && make install
+  cp examples/systemd/vnstat.service /etc/systemd/system/
+  systemctl daemon-reload
+  systemctl restart vnstat
+  cd .. && rm -r vnstat
+}
+
+main
