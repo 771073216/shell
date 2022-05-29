d5ec5b5f3036445bef93566b0654d282c6ce2dfa
771073216 <771073216@qq.com>
2022-05-29 16:22:36 +0800

    update

--- vnstat.sh
+++ vnstat.sh
@@ -1,32 +1,23 @@
 #!/usr/bin/env bash
-remote_ver=$(curl -sSL https://api.github.com/repos/vergoh/vnstat/releases/latest | awk -F'"' '/tag_name/{print$4}' | tr -d v)
+[[ $EUID -ne 0 ]] && echo -e "请以root身份执行该脚本！" && exit 1
+
 main() {
   if command -v vnstat; then
     update
     return
   fi
-  apt install -y build-essential libsqlite3-dev git
-  curl -L https://codeload.github.com/vergoh/vnstat/zip/refs/tags/v"${remote_ver}" && cd vnstat || exit 1
-  ./configure --prefix=/usr --sysconfdir=/etc && make && make install
-  cp examples/systemd/vnstat.service /etc/systemd/system/
-  systemctl enable vnstat.service --now
-  cd .. && rm -r vnstat
+  curl -L https://raw.githubusercontent.com/771073216/deb/main/vnstat.deb -o vnstat.deb
+  dpkg -i vnstat.deb
 }
 
 update() {
   local_ver=$(vnstat -v | awk '{print$2}')
+  remote_ver=$(curl -sSL https://raw.githubusercontent.com/771073216/deb/main/version | awk '/vnstat/{print$2}')
   if [ "${local_ver}" == "${remote_ver}" ]; then
     return
   fi
-  [ -e vnstat ] && rm -r vnstat
-  curl -L https://codeload.github.com/vergoh/vnstat/zip/refs/tags/v"${remote_ver}" -o vnstat.zip
-  unzip vnstat.zip
-  cd vnstat-"$remote_ver" || exit 1
-  ./configure --prefix=/usr --sysconfdir=/etc && make && make install
-  cp examples/systemd/vnstat.service /etc/systemd/system/
-  systemctl daemon-reload
-  systemctl restart vnstat
-  cd .. && rm -r vnstat-"$remote_ver"
+  curl -L https://raw.githubusercontent.com/771073216/deb/main/vnstat.deb -o vnstat.deb
+  dpkg -i vnstat.deb
 }
 
 main
