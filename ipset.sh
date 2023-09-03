f9ecfea974b66395cdd2f35db60243bba0e52a12
771073216 <771073216@qq.com>
2023-09-03 11:04:38 +0800

    update

--- ipset.sh
+++ ipset.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-[[ $EUID -ne 0 ]] && echo "请以root身份执行该脚本！" && exit 1
-# ipset create whitelist hash:net maxelem 1000000
+set -e
+
 wget https://api.azzb.club/https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china.txt -O /tmp/china.txt
 ipset flush whitelist
 ip=$(cat /tmp/china.txt)
 for i in $ip; do
   ipset add whitelist "$i"
 done
+ipset save whitelist -f /home/ubuntu/white.ipset
--- iptables.sh
+++ iptables.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
+set -e
 [[ $EUID -ne 0 ]] && echo "请以root身份执行该脚本！" && exit 1
+
+ipset restore -f /home/ubuntu/white.ipset
+
 iptables -F
-iptables -A INPUT -m set --match-set whitelist src -j ACCEPT
-iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -m set ! --match-set whitelist src -j REJECT --reject-with icmp-host-prohibited
+
 iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 5000 -j ACCEPT
-iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 10000:10001 -j ACCEPT
+iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 11000:11001 -j ACCEPT
 iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 12000:12001 -j ACCEPT
 iptables -A INPUT -j REJECT --reject-with icmp-host-prohibited
 iptables -A FORWARD -j REJECT --reject-with icmp-host-prohibited
-netfilter-persistent save
