836a12349c1ff7a968f4fe846bd73a767221e26f
771073216 <771073216@qq.com>
2023-09-28 14:36:57 +0800

    update

--- sing-box.sh
+++ sing-box.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 latest_ver=$(curl -s https://api.github.com/repos/sagernet/sing-box/tags | awk -F'"' '/name/{print$4}' | sort -n | tail -n1)
 
 main() {
@@ -17,7 +18,7 @@ main() {
 
 install_sing_box() {
   ver=$(echo "$latest_ver" | cut -c 2-)
-  wget https://github.com/SagerNet/sing-box/releases/download/"$latest_ver"/sing-box_"$ver"_linux_amd64.tar.gz -O /tmp/sing-box.tar.gz
+  wget https://github.com/SagerNet/sing-box/releases/download/"$latest_ver"/sing-box-"$ver"-linux-amd64.tar.gz -O /tmp/sing-box.tar.gz
   tar -xf /tmp/sing-box.tar.gz -C /tmp/
   install /tmp/sing-box-"$ver"-linux-amd64/sing-box /usr/local/bin/
   rm -r /tmp/sing-box-"$ver"-linux-amd64 /tmp/sing-box.tar.gz
@@ -28,3 +29,4 @@ if [ "$1" == "-f" ]; then
   exit 0 
 fi
 main
+
