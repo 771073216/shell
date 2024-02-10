9778738c63a670f5752b7b225f24548a9e129edb
771073216 <37037387+771073216@users.noreply.github.com>
2024-02-10 22:14:36 +0800

    Update ipset.sh

--- ipset.sh
+++ ipset.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 [[ $EUID -ne 0 ]] && echo "请以root身份执行该脚本！" && exit 1
-wget https://api.azzb.club/https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china.txt -O /tmp/china.txt
+wget https://api.iristory.top/https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china.txt -O /tmp/china.txt
 ipset flush whitelist
 ip=$(cat /tmp/china.txt)
 for i in $ip; do
